6dc0f40576c3d3f9b28eb433f67719717abf1756
Makeeva Anastasiia <makeevaa@biocad.ru>
2022-08-05 17:13:55 +0300

    last change friday

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 
 
-def read_file():
+def read_file() -> pd.DataFrame:
     data_path = 'C:\\Users\\makeevaa\\2021-04-24_BCD132-4_Bmem2_406-1_408-1_411-1.csv'
     data = pd.read_csv(data_path, sep = ';')
     data = data.drop(labels = 0, axis = 0) #это можно вернуть как изначальные данные если склеить с другими таблицами
@@ -11,7 +11,13 @@ def read_file():
     temp = data.copy()
     temp.columns = temp.iloc[0]
     temp = temp.drop(labels = 1, axis=0)#таблица без названия эксперимента, все расчеты дальше с ней
-    compute(temp) #считает все таблицы
+    names = ['1-3 B-cells Events', '1-5 Plasm 1 Events', '1-4 Bmem Events', 'naive Events'] #дочерние популяции
+    parent = '1-4 Lymph Events'
+    temp[parent] = temp[parent].astype('int')
+    for i in names:
+        temp[i] = temp[i].astype('int')
+
+    return(temp, names, parent)
 
 def biotable(temp): #таблица учета биообразцов
     table = pd.DataFrame(columns=('PD-1', 'PD-2', 'PD-3', 'PD-4', 'PD-5', 'PD-6', 'PD-7'))  # надо будет задавать количество столбцов
@@ -46,43 +52,61 @@ def comp_percent(temp, child, parent): #возвращает таблицу с %
             table.loc[int(lot_pd[0]), PD] = sum / kol
     return(table)
 
-def krit(temp):
-    table = pd.DataFrame(columns=('% CV B-cells', '% CV B-mem', '% CV Plasm', '% CV naive'))
-    i = 0
-    while i < len(temp):
-        row = temp.iloc[i]
-        if row['Sample ID:'] in table.index:
-            continue
-        else:
-            ID = row['Sample ID:']
-            sum = 0
-            kol = 0
-            while (row['Sample ID:'] == ID and i < len(temp)):
-                if 'rep' in row['Tube Name:']:
-                    ans = (int(row[child]) / int(row[parent])) * 100
-                    sum += ans
-                    kol += 1
-                    #посчитать std для всех и mean для всех
-                i += 1
-                if i < len(temp):
-                    row = temp.iloc[i]
-            lot_pd = ID.split('-')
-            PD = 'PD-' + lot_pd[1]
-            table.loc[int(lot_pd[0]), PD] = sum / kol
-    return (table)
+def comp_cv(df: pd.DataFrame, child, parent): #может возвращать cv, а может сразу результат пригодности(сравнить с limit)
+    df[child] = df[child] / df[parent] * 100
+    mean = df[child].mean()
+    sd = df[child].std()
+    return(sd / mean * 100)
 
+def krit(df : pd.DataFrame, names, parent): #собирает таблицу из всех критериев
+    table = pd.DataFrame(columns=('ЛОТ', 'Точка PD', '%CV 1-3 B-cells Events', '%CV 1-4 Bmem Events', '%CV 1-5 Plasm 1 Events', '%CV naive Events'))#пока что нужно, чтобы названия колонок содержали названия популяций, потом можно будет это как-то проверять или сделать таблицу соответствий
+    df = df.loc[df['Tube Name:'].str.contains('rep')]
+    group = df.groupby('Sample ID:')
+    list_group = list(group)
+    for i in list_group:
+        lot_pd = i[0].split('-')
+        PD = 'PD-' + lot_pd[1]
+        table.loc[int(lot_pd[0]) * 10 + int(lot_pd[1]), 'ЛОТ'] = lot_pd[0]
+        table.loc[int(lot_pd[0]) * 10 + int(lot_pd[1]), 'Точка PD'] = lot_pd[1]
+        for j in names:
+            cv = comp_cv(i[1], j, parent)
+            for col in table.columns:
+                if j in col:
+                    table.loc[int(lot_pd[0]) * 10 + int(lot_pd[1]), col] = cv #индексы странные зато уникальные
+    return(table)
 
-def compute(temp): #запускает все функции подсчета таблиц и выводит их
+def compute(temp, names, parent): #запускает все функции подсчета таблиц и выводит их
     temp = temp.sort_values(by = 'Sample ID:')
     table = biotable(temp)
     print('Учет биообразцов', table, sep = '\n')
-    b_cells = comp_percent(temp, '1-3 B-cells Events', '1-4 Lymph Events')
-    plasm = comp_percent(temp, '1-5 Plasm 1 Events', '1-4 Lymph Events')
-    Bmem = comp_percent(temp, '1-4 Bmem Events', '1-4 Lymph Events')
-    naive = comp_percent(temp, 'naive Events', '1-4 Lymph Events')
-    print('Mean % 1-3 B-cells in 1-4 Lymph', b_cells, sep = '\n') #Mean % 1-3 B-cells in 1-4 Lymph
+    for i in names:  #если не нужно хранить 4 отдельных датафрейма, или же можно записать их в общую структуру
+        table = comp_percentgb(temp, i, parent)
+        print('Mean %', i, 'in', parent)
+        print(table)
+    '''b_cells = comp_percentgb(temp, '1-3 B-cells Events', '1-4 Lymph Events')
+    plasm = comp_percentgb(temp, '1-5 Plasm 1 Events', '1-4 Lymph Events')
+    Bmem = comp_percentgb(temp, '1-4 Bmem Events', '1-4 Lymph Events')
+    naive = comp_percentgb(temp, 'naive Events', '1-4 Lymph Events')'''
+    '''print('Mean % 1-3 B-cells in 1-4 Lymph', b_cells, sep = '\n') #Mean % 1-3 B-cells in 1-4 Lymph
     print('Mean % 1-5 Plasm 1 in 1-4 Lymph', plasm, sep = '\n') #Mean % 1-5 Plasm 1 in 1-4 Lymph
     print('Mean % 1-4 Bmem in 1-4 Lymph', Bmem, sep = '\n') #Mean % 1-4 Bmem in 1-4 Lymph
     print('Mean % naïve in 1-4 Lymph', naive, sep = '\n')  #Mean % naïve in 1-4 Lymph'''
+    krit_table = krit(temp, names, parent)
+    print('Критерии пригодности', krit_table, sep = '\n')
+def comp_percentgb(df : pd.DataFrame, child, parent):
+    df = df.loc[df['Tube Name:'].str.contains('rep')]
 
-read_file()
+    group = df.groupby('Sample ID:')
+    list_group = list(group)
+    table = pd.DataFrame(columns=('PD-1', 'PD-2', 'PD-3', 'PD-4', 'PD-5', 'PD-6', 'PD-7'))
+    for i in list_group:
+        i[1][child] = i[1][child] / i[1][parent] * 100
+        lot_pd = i[0].split('-')
+        PD = 'PD-' + lot_pd[1]
+        table.loc[int(lot_pd[0]), PD] = i[1][child].mean()
+    return(table)
+
+data = read_file()
+compute(data[0], data[1], data[2])
+
+#compute(read_file())
