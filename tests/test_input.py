a16a8563b630f2ec5b218a8fd13a1149bd49bbb7
Pavel Kuznetsov <kuznetsovpm@biocad.ru>
2022-08-10 16:08:28 +0300

    projcet root example

--- tests/test_input.py
+++ tests/test_input.py
@@ -1,7 +1,19 @@
 import pandas as pd
 
+from pathlib import Path
+
 from fctool.main import comp_cv
 
+
+
+def p_root() -> Path:
+    cur_file = Path(__file__)
+
+    return cur_file.parent.parent
+
+
+def test_proot():
+    print(p_root() / "my.csv")
 
 cv_inp1 = pd.DataFrame({
     "child": [1, 1, 1],
